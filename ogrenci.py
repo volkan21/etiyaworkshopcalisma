d03384a128afd5d369ab002a03cb65a458a0f52c
volkan21 <volkanerdogus@gmail.com>
2023-04-11 17:33:04 +0300

    Create ogrenci.py

---
+++ ogrenci.py
@@ -0,0 +1,9 @@
+class Ogrenci:
+    def __init__(self,ad,soyad):
+        self.ad=ad
+        self.soyad=soyad
+        
+    def goster(self):
+        print(f"Öğrencinin adi:{self.ad}")
+        print(f"Öğrencinin Soyadi:{self.soyad}")
+     
