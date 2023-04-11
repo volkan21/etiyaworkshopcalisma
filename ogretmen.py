64b5785c62c01a2bd97c43a47ba952a7163bbc47
volkan21 <volkanerdogus@gmail.com>
2023-04-11 17:33:23 +0300

    Create ogretmen.py

---
+++ ogretmen.py
@@ -0,0 +1,9 @@
+class Ogretmen:
+        def __init__(self,ad,soyad):
+         self.ad=ad
+         self.soyad=soyad
+        
+        def goster(self):
+            print(f"Öğretmenin adi:{self.ad}")
+            print(f"Öğretmenin soyadi:{self.soyad}")
+          
